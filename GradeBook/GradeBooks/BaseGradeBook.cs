4b2cf279bdc972eed59d749d3568f605e5121213
Robin Borg <robin.borg@triona.se>
2020-09-15 11:33:58 +0200

    Step 7

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -14,9 +14,11 @@ namespace GradeBook.GradeBooks
         public string Name { get; set; }
         public List<Student> Students { get; set; }
         public GradeBookType Type { get; set; }
+        public bool IsWeighted { get; set; }
 
-        public BaseGradeBook(string name)
+        public BaseGradeBook(string name, bool weighted)
         {
+            IsWeighted = weighted;
             Name = name;
             Students = new List<Student>();
         }
@@ -107,21 +109,41 @@ namespace GradeBook.GradeBooks
 
         public virtual double GetGPA(char letterGrade, StudentType studentType)
         {
+           if(IsWeighted && studentType == StudentType.Honors || studentType == StudentType.DualEnrolled)
+            {
             switch (letterGrade)
             {
                 case 'A':
-                    return 4;
+                    return 5;
                 case 'B':
-                    return 3;
+                    return 4;
                 case 'C':
-                    return 2;
+                    return 3;
                 case 'D':
-                    return 1;
+                    return 2;
                 case 'F':
-                    return 0;
+                    return 1;
             }
             return 0;
+        } else
+            {
+                switch (letterGrade)
+                {
+                    case 'A':
+                        return 4;
+                    case 'B':
+                        return 3;
+                    case 'C':
+                        return 2;
+                    case 'D':
+                        return 1;
+                    case 'F':
+                        return 0;
+                }
+                return 0;
+            }
         }
+
 
         public virtual void CalculateStatistics()
         {

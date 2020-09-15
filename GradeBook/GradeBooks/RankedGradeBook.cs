84ab8a8d80c31d634a189d044f9790b565a08418
Robin Borg <robin.borg@triona.se>
2020-09-15 10:22:48 +0200

    Ranked grades

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,6 +1,7 @@
 ﻿using GradeBook.Enums;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace GradeBook.GradeBooks
@@ -10,6 +11,25 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Ranked;
+        }
+        override public char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+                throw new InvalidOperationException("Ranked grading requires at least 5 students");
+
+            var threshhold = (int)Math.Ceiling(Students.Count * 0.2);
+            var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
+
+            if (grades[threshhold - 1] <= averageGrade)
+                return 'A';
+            else if (grades[(threshhold * 2) - 1] <= averageGrade)
+                return 'B';
+            else if (grades[(threshhold * 3) - 1] <= averageGrade)
+                return 'C';
+            else if (grades[(threshhold * 4) - 1] <= averageGrade)
+                return 'D';
+            else
+            return 'F';
         }
     }
 }
